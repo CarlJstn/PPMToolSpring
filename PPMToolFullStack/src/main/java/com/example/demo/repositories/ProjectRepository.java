4cd91dffc5ad0e591939c1ee866569a4f760f514
carljustinedg@gmail.com <carljustinedg@gmail.com>
2019-07-31 14:02:33 +0800

    get project by id

--- PPMToolFullStack/src/main/java/com/example/demo/repositories/ProjectRepository.java
+++ PPMToolFullStack/src/main/java/com/example/demo/repositories/ProjectRepository.java
@@ -8,4 +8,5 @@ import com.example.demo.domain.Project;
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long>{
 
+	Project findByProjectIdentifier(String projectId);
 }
