4e32a1e3e300c873621489337c10487ee3f3b7c6
carljustinedg@gmail.com <carljustinedg@gmail.com>
2019-08-02 15:40:20 +0800

    add project form

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
+import AddProject from './components/Project/AddProject';
 
 function App() {
   return (
-    <div className='App'>
-      <Dashboard />
-    </div>
+    <Router>
+      <div className='App'>
+        <Header />
+        <Route exact path='/dashboard' component={Dashboard} />
+        <Route exact path='/addProject' component={AddProject} />
+      </div>
+    </Router>
   );
 }
 
--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+
 import ProjectItem from './Project/ProjectItem';
+import CreateProjectButton from './Project/CreateProjectButton';
 
 class Dashboard extends Component {
   render() {
+    let ProjectItems = <ProjectItem />;
+
     return (
-      <div>
-        <h1>Welcome to the dashboard</h1>
-        <ProjectItem />
+      <div className='projects'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-12'>
+              <h1 className='display-4 text-center'>Projects</h1>
+              <br />
+              <CreateProjectButton />
+              <br />
+              <hr />
+              {ProjectItems}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
